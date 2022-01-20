eb101a0f171ba5d0cc91c0a952fc0a284e555918
andreas <benzan81@gmail.com>
2022-01-20 14:46:00 +1300

    show index.html via server

--- routes.js
+++ routes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const response = require('./server')
 const router = express.Router()
+const path = require('path');
 
 router.get('/', (req, res) => {
-  res.send('Hello xxx World')
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+
+  // response.sendFile(path.join(__dirname, 'banana', 'greeting.html'))
+
 })
 
 module.exports = router
