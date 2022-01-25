d01ba802c8cfd70b104eca88d41cbd5732c33ea9
andreas <benzan81@gmail.com>
2022-01-25 19:43:39 +1300

    clean up code

--- routes.js
+++ routes.js
@@ -2,18 +2,16 @@ const express = require('express')
 const response = require('./server')
 const router = express.Router()
 const func = require('./functions')
-
 const path = require('path')
 const cheerio = require('cheerio')
-
 
 router.get('/', (req, res) => {
   func.getAllTasks()
    .then((task) => {
-    const viewdata = {
-      tasks: task,
-    }
-    res.render('mahiList', viewdata)
+      const viewdata = {
+        tasks: task,
+      }
+      res.render('mahiList', viewdata)
    })
 })
 
@@ -29,26 +27,22 @@ router.post('/:t_id/delete',(req, res) => {
 router.get('/:t_id/edit', (req, res) => {
   //grabs the Id from the URL as a string btw
   const t_id = Number(req.params.t_id)
-
   const viewdata = {}
 
   func.getOneTask(t_id)
   .then((task) => {
     viewdata.task = task
-      func.getAllUsers()
-        .then((users) => {
-          viewdata.users = users
-          res.render('mahiEdit', viewdata)
-        })
-
+    func.getAllUsers()
+      .then((users) => {
+        viewdata.users = users
+        res.render('mahiEdit', viewdata)
+      })
    })
 })
 
 router.post('/:t_id/edit',(req, res) => {
-  
   const t_id = Number(req.params.t_id)
   const editedTask = req.body
-  
   const updatedTask = {
     title: editedTask.title,
     description: editedTask.description,
@@ -62,20 +56,17 @@ router.post('/:t_id/edit',(req, res) => {
 })
 
 router.post('/add',(req, res) => {
-
   const sampleTitle = "new Task"
   const viewdata = {}
 
   func.addTask(sampleTitle)
-
    .then((task) => {
-   viewdata.task = task
-   func.getAllUsers()
+      viewdata.task = task
+      func.getAllUsers()
         .then((users) => {
           viewdata.users = users
-   
-   res.render('mahiEdit', viewdata)
-  })
+          res.render('mahiEdit', viewdata)
+      })
    })
 })
 
